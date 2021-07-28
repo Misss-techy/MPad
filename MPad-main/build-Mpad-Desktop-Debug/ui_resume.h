47ca5fab1b58560001b0eb3712add7bfbd40af0a
Mushina <61909434+Misss-techy@users.noreply.github.com>
2021-07-28 21:57:39 +0530

    Add files via upload

---
+++ MPad-main/build-Mpad-Desktop-Debug/ui_resume.h
@@ -0,0 +1,212 @@
+/********************************************************************************
+** Form generated from reading UI file 'resume.ui'
+**
+** Created by: Qt User Interface Compiler version 5.12.8
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_RESUME_H
+#define UI_RESUME_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QDialog>
+#include <QtWidgets/QLabel>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Resume
+{
+public:
+    QLabel *label;
+    QLabel *label_2;
+    QLabel *label_3;
+    QLabel *label_4;
+    QLabel *label_5;
+    QLabel *label_6;
+    QLabel *label_7;
+    QLabel *label_8;
+    QLabel *label_9;
+    QLabel *label_10;
+    QLabel *label_11;
+    QLabel *label_12;
+    QLabel *label_13;
+    QLabel *label_14;
+    QLabel *label_15;
+    QLabel *label_16;
+    QLabel *label_17;
+    QLabel *label_18;
+    QLabel *label_19;
+    QLabel *label_20;
+    QLabel *label_21;
+    QLabel *label_22;
+    QLabel *label_23;
+    QLabel *label_24;
+
+    void setupUi(QDialog *Resume)
+    {
+        if (Resume->objectName().isEmpty())
+            Resume->setObjectName(QString::fromUtf8("Resume"));
+        Resume->resize(764, 776);
+        Resume->setAutoFillBackground(false);
+        Resume->setStyleSheet(QString::fromUtf8(""));
+        Resume->setSizeGripEnabled(false);
+        label = new QLabel(Resume);
+        label->setObjectName(QString::fromUtf8("label"));
+        label->setGeometry(QRect(20, 10, 361, 51));
+        QFont font;
+        font.setFamily(QString::fromUtf8("Nirmala UI Semilight"));
+        font.setPointSize(13);
+        font.setBold(false);
+        font.setUnderline(false);
+        font.setWeight(50);
+        label->setFont(font);
+        label_2 = new QLabel(Resume);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+        label_2->setGeometry(QRect(0, -50, 771, 861));
+        label_3 = new QLabel(Resume);
+        label_3->setObjectName(QString::fromUtf8("label_3"));
+        label_3->setGeometry(QRect(0, 0, 771, 71));
+        label_4 = new QLabel(Resume);
+        label_4->setObjectName(QString::fromUtf8("label_4"));
+        label_4->setGeometry(QRect(20, 80, 551, 211));
+        label_5 = new QLabel(Resume);
+        label_5->setObjectName(QString::fromUtf8("label_5"));
+        label_5->setGeometry(QRect(20, 90, 191, 31));
+        label_6 = new QLabel(Resume);
+        label_6->setObjectName(QString::fromUtf8("label_6"));
+        label_6->setGeometry(QRect(20, 260, 411, 31));
+        label_7 = new QLabel(Resume);
+        label_7->setObjectName(QString::fromUtf8("label_7"));
+        label_7->setGeometry(QRect(20, 290, 231, 31));
+        label_8 = new QLabel(Resume);
+        label_8->setObjectName(QString::fromUtf8("label_8"));
+        label_8->setGeometry(QRect(20, 310, 531, 91));
+        label_9 = new QLabel(Resume);
+        label_9->setObjectName(QString::fromUtf8("label_9"));
+        label_9->setGeometry(QRect(20, 280, 481, 16));
+        label_10 = new QLabel(Resume);
+        label_10->setObjectName(QString::fromUtf8("label_10"));
+        label_10->setGeometry(QRect(20, 400, 221, 16));
+        label_11 = new QLabel(Resume);
+        label_11->setObjectName(QString::fromUtf8("label_11"));
+        label_11->setGeometry(QRect(20, 500, 481, 16));
+        label_12 = new QLabel(Resume);
+        label_12->setObjectName(QString::fromUtf8("label_12"));
+        label_12->setGeometry(QRect(20, 400, 531, 91));
+        label_13 = new QLabel(Resume);
+        label_13->setObjectName(QString::fromUtf8("label_13"));
+        label_13->setGeometry(QRect(20, 480, 491, 31));
+        label_14 = new QLabel(Resume);
+        label_14->setObjectName(QString::fromUtf8("label_14"));
+        label_14->setGeometry(QRect(20, 520, 221, 16));
+        label_15 = new QLabel(Resume);
+        label_15->setObjectName(QString::fromUtf8("label_15"));
+        label_15->setGeometry(QRect(20, 540, 531, 91));
+        label_16 = new QLabel(Resume);
+        label_16->setObjectName(QString::fromUtf8("label_16"));
+        label_16->setGeometry(QRect(20, 630, 491, 31));
+        label_17 = new QLabel(Resume);
+        label_17->setObjectName(QString::fromUtf8("label_17"));
+        label_17->setGeometry(QRect(20, 650, 481, 16));
+        label_18 = new QLabel(Resume);
+        label_18->setObjectName(QString::fromUtf8("label_18"));
+        label_18->setGeometry(QRect(20, 650, 521, 61));
+        label_19 = new QLabel(Resume);
+        label_19->setObjectName(QString::fromUtf8("label_19"));
+        label_19->setGeometry(QRect(20, 660, 531, 91));
+        label_20 = new QLabel(Resume);
+        label_20->setObjectName(QString::fromUtf8("label_20"));
+        label_20->setGeometry(QRect(20, 710, 531, 91));
+        label_21 = new QLabel(Resume);
+        label_21->setObjectName(QString::fromUtf8("label_21"));
+        label_21->setGeometry(QRect(20, 720, 521, 61));
+        label_22 = new QLabel(Resume);
+        label_22->setObjectName(QString::fromUtf8("label_22"));
+        label_22->setGeometry(QRect(620, 120, 151, 661));
+        label_23 = new QLabel(Resume);
+        label_23->setObjectName(QString::fromUtf8("label_23"));
+        label_23->setGeometry(QRect(640, 150, 111, 31));
+        label_24 = new QLabel(Resume);
+        label_24->setObjectName(QString::fromUtf8("label_24"));
+        label_24->setGeometry(QRect(640, 200, 121, 231));
+        label_2->raise();
+        label_3->raise();
+        label->raise();
+        label_4->raise();
+        label_5->raise();
+        label_6->raise();
+        label_7->raise();
+        label_8->raise();
+        label_9->raise();
+        label_10->raise();
+        label_11->raise();
+        label_12->raise();
+        label_13->raise();
+        label_14->raise();
+        label_15->raise();
+        label_16->raise();
+        label_17->raise();
+        label_18->raise();
+        label_19->raise();
+        label_20->raise();
+        label_21->raise();
+        label_22->raise();
+        label_23->raise();
+        label_24->raise();
+
+        retranslateUi(Resume);
+
+        QMetaObject::connectSlotsByName(Resume);
+    } // setupUi
+
+    void retranslateUi(QDialog *Resume)
+    {
+        Resume->setWindowTitle(QApplication::translate("Resume", "Resume", nullptr));
+        label->setText(QApplication::translate("Resume", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:'Nirmala UI Semilight'; font-size:13pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:20pt; font-weight:600; color:#ffffff;\">Sumaiyathun Muhshina </span></p></body></html>", nullptr));
+        label_2->setText(QApplication::translate("Resume", "<html><head/><body><p><img src=\":/Images/white.jpg\"/></p></body></html>", nullptr));
+        label_3->setText(QApplication::translate("Resume", "<html><head/><body><p><img src=\":/Images/solid-light-blue-background.jpg\"/></p></body></html>", nullptr));
+        label_4->setText(QApplication::translate("Resume", "<html><head/><body><table border=\"2\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px;\" cellspacing=\"3\" cellpadding=\"3\"><tr><td><p><span style=\" font-weight:600;\">Qualification</span></p></td><td><p><span style=\" font-weight:600;\">Institue/Board</span></p></td><td><p><span style=\" font-weight:600;\">Score</span></p></td></tr><tr><td><p>B.Tech. Computer Science and Engineering </p></td><td><p>Crescent University</p></td><td><p>9.1CGPA </p></td></tr><tr><td><p>Senior Secondary </p></td><td><p>Trinity Academy/CBSE </p></td><td><p>79%</p></td></tr><tr><td><p>Secondary </p></td><td><p>Trinity Academy/CBSE </p></td><td><p>10CGPA </p></td></tr></table></body></html>", nullptr));
+        label_5->setText(QApplication::translate("Resume", "<html><head/><body><h3 style=\" margin-top:16px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:x-large;\">EDUCATION</span></h2><h2 style=\"-qt-paragraph-type:empty; margin-top:16px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:x-large; font-weight:600;\"><br/></h3></body></html>", nullptr));
+        label_6->setText(QApplication::translate("Resume", "<html><head/><body><h2=3 style=\" margin-top:16px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:x-large;\">WORK AND LEADERSHIP EXPERIENCE</span></h2><h2 style=\"-qt-paragraph-type:empty; margin-top:16px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:x-large;\"><br/></h3></body></html>", nullptr));
+        label_7->setText(QApplication::translate("Resume", "<h3>Software Developer Intern</h3>", nullptr));
+        label_8->setText(QApplication::translate("Resume", "<html><head/><body><p><span style=\" font-size:7pt; font-style:italic;\">Valanad, Thanjavur, Tn / March - June, 2020</span></p><p>\342\200\242 Assisted in Designing and Coding of a minimal Business software using VB.NET.</p><p>\342\200\242 Created and managed databases using MySQL and carried out data operations.</p></body></html>", nullptr));
+        label_9->setText(QApplication::translate("Resume", "<html><head/><body><hr>\n"
+"</body></html>", nullptr));
+        label_10->setText(QApplication::translate("Resume", "<html><head/><body><h3 style=\" margin-top:14px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:large; font-weight:600;\">Under Secratary General</span></h3><h3 style=\"-qt-paragraph-type:empty; margin-top:14px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:large; font-weight:600;\"><br/></h3></body></html>", nullptr));
+        label_11->setText(QApplication::translate("Resume", "<html><head/><body><hr>\n"
+"</body></html>", nullptr));
+        label_12->setText(QApplication::translate("Resume", "<html><head/><body><p><span style=\" font-size:7pt; font-style:italic;\">Crescent Model United Nation / July,2020 - Present</span></p><p>\342\200\242 Organizing Intra-MUN and monitoring all operations and supervising Board members.</p></body></html>", nullptr));
+        label_13->setText(QApplication::translate("Resume", "<html><head/><body><h2 style=\" margin-top:16px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:x-large; font-weight:600;\">ROLES, RESPONSIBILITES AND VOLUNTEERING</span></h2><h2 style=\"-qt-paragraph-type:empty; margin-top:16px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:x-large; font-weight:600;\"><br/></h2></body></html>", nullptr));
+        label_14->setText(QApplication::translate("Resume", "<html><head/><body><h3 style=\" margin-top:14px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:large; font-weight:600;\">Campus Ambassador</span></h3><h3 style=\"-qt-paragraph-type:empty; margin-top:14px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:large; font-weight:600;\"><br/></h3></body></html>", nullptr));
+        label_15->setText(QApplication::translate("Resume", "<html><head/><body><p><span style=\" font-size:7pt; font-style:italic;\">Unshool, Hyderabad / December,2020 - January, 2021</span></p><p>\342\200\242 Interact with students and represent the company around campus.</p><p>\342\200\242 Market research to identify the target market for the products.</p></body></html>", nullptr));
+        label_16->setText(QApplication::translate("Resume", "<html><head/><body><h2 style=\" margin-top:16px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:x-large; font-weight:600;\">CERTIFICATIONS</span></h2><h2 style=\"-qt-paragraph-type:empty; margin-top:16px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:x-large; font-weight:600;\"><br/></h2></body></html>", nullptr));
+        label_17->setText(QApplication::translate("Resume", "<html><head/><body><hr>\n"
+"</body></html>", nullptr));
+        label_18->setText(QApplication::translate("Resume", "<html><head/><body><h3 style=\" margin-top:14px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:large; font-weight:600;\">Industrial Control System Cybersecurity Training</span></h3></body></html>", nullptr));
+        label_19->setText(QApplication::translate("Resume", "<html><head/><body><p><span style=\" font-size:12pt; font-style:italic;\">U.S. Department of Homeland Security and CISA / December 2020</span></p></body></html>", nullptr));
+        label_20->setText(QApplication::translate("Resume", "<html><head/><body><p><span style=\" font-style:italic;\">IBM / January 2021 - Present</span></p></body></html>", nullptr));
+        label_21->setText(QApplication::translate("Resume", "<html><head/><body><h3 style=\" margin-top:14px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:large; font-weight:600;\">AI ANALYST</span></h3><h3 style=\"-qt-paragraph-type:empty; margin-top:14px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:large; font-weight:600;\"><br/></h3><h3 style=\"-qt-paragraph-type:empty; margin-top:14px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:large; font-weight:600;\"><br/></h3></body></html>", nullptr));
+        label_22->setText(QApplication::translate("Resume", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:'MS Shell Dlg 2'; font-size:8pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><img src=\":/Images/grey.jpg\" /></p></body></html>", nullptr));
+        label_23->setText(QApplication::translate("Resume", "<html><head/><body><h3 style=\" margin-top:14px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:large; font-weight:600;\">Skills</span></h3><h3 style=\"-qt-paragraph-type:empty; margin-top:14px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:large; font-weight:600;\"><br/></h3></body></html>", nullptr));
+        label_24->setText(QApplication::translate("Resume", "<html><head/><body><p>\342\200\242 C++</p><p>\342\200\242 .NET</p><p>\342\200\242 R Programming</p><p>\342\200\242 Linux</p><p>\342\200\242 HTML</p><p>\342\200\242 CSS</p><p>\342\200\242 MySQL</p><p>\342\200\242 Python</p><p><br/></p><p><br/></p><p><br/></p></body></html>", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Resume: public Ui_Resume {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_RESUME_H
